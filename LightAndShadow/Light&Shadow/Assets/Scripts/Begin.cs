2729e3af7b5b4013e3d5ef6480e0a00a408defa0
XHL <2682950883@qq.com>
2020-05-14 20:18:11 +0800

    Add Scene0 UI

--- LightAndShadow/Light&Shadow/Assets/Scripts/Begin.cs
+++ LightAndShadow/Light&Shadow/Assets/Scripts/Begin.cs
@@ -5,8 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class Begin : MonoBehaviour
 {
+    private GameObject UI_begin;
+    private GameObject UI_settings;
+    private void Start()
+    {
+        UI_begin = GameObject.FindWithTag("UI_Begin");
+        UI_settings = GameObject.FindWithTag("UI_Settings");
+        UI_settings.SetActive(false);
+    }
     public void LoadGame()
     {
         SceneManager.LoadScene(1);
+    }
+    public void SettingsOnClick()
+    {
+        UI_begin.SetActive(false);
+        UI_settings.SetActive(true);
+    }
+    public void BackOnClick()
+    {
+        UI_begin.SetActive(true);
+        UI_settings.SetActive(false);
+    }
+    public void ExitOnClick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
